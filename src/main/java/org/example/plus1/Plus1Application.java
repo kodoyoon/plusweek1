00af116e014da4579110d185515a8174b59607ca
doyoon <doyoun0527@naver.com>
2023-12-20 17:49:05 +0900

    init commit

---
+++ src/main/java/org/example/plus1/Plus1Application.java
@@ -0,0 +1,13 @@
+package org.example.plus1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plus1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Plus1Application.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/plus1/Plus1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.plus1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plus1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
